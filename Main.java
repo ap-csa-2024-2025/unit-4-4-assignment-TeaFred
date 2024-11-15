1f816c929c130e47bf09c46d12915aa264b82c03
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-11-15 21:50:14 +0000

    save

--- Main.java
+++ Main.java
@@ -5,13 +5,18 @@ public class Main
 {
   public static void main(String[] args)
   {
-    //Problem 1
     Scanner sc = new Scanner (System.in);
+    boolean doProb1 = true;
+    boolean doProb2 = false;
+    boolean doProb3 = false;
+
+    if (doProb1){
+    //Problem 1
     System.out.println("Please enter a String: ");
     String theWord = sc.nextLine();
     int theCount = 0;
     theWord = theWord.toLowerCase();
-    for (int i = 0; i < theWord.length(); i++)
+    for (int i = 0; i < theWord.length()-1; i++)
     {
       if ((theWord.substring(i,i+1).equals("p")) && (((theWord.substring(i+1,i+2).equals("a")) ||(theWord.substring(i+1,i+2).equals("e")) ||(theWord.substring(i+1,i+2).equals("i")) ||(theWord.substring(i+1,i+2).equals("o")) ||(theWord.substring(i+1,i+2).equals("u")))))
       {
@@ -19,13 +24,16 @@ public class Main
       }
     }
     System.out.println("Contains p followed by a vowel " + theCount + " times.");
+    }
+    if (doProb2)
+    {
     //Problem 2
     System.out.println("Please enter a String: ");
     String theWord2 = new String();
     theWord2 = sc.nextLine();
     theWord2 = theWord2.toLowerCase();
     String theWord3 = new String();
-    for (int i = 0; i < theWord.length(); i++)
+    for (int i = 0; i < theWord2.length(); i++)
     {
       if (!(theWord2.substring(i, i+1).equals("e")) && !(theWord2.substring(i, i+1).equals("t")) 
       && !(theWord2.substring(i, i+1).equals("a")) 
@@ -36,11 +44,28 @@ public class Main
       }
     }
     System.out.println(theWord3);
-  }
-
-  public static boolean isPrime(int N)
-  {
-    // TODO: Replace and write your code below
-    return false;
+    }
+    if (doProb3)
+    {
+      System.out.println("Please enter two strings: ");
+      String theWord4 = new String();
+      String theWord5 = new String();
+      String theWord6 = new String();
+      theWord4 = sc.nextLine();
+      theWord5 = sc.nextLine();
+      if (theWord4.length() == theWord5.length())
+      {
+        for (int i = theWord4.length()-1; i >= 0; i--)
+        {
+          theWord6 += theWord5.substring(i,i+1);
+          theWord6 += theWord4.substring(i,i+1);
+        }
+      }
+      else
+      {
+        System.out.println("error");
+      }
+      System.out.println(theWord6);
+    }
   }
 }
